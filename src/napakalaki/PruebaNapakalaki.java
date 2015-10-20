beb692b1519bfbc094969e395ea0ff6863e35df9
David Vargas <dvcarrillo@correo.ugr.es>
2015-10-20 17:59:31 +0200

    Added all the definitions of monsters in the main class
    Monsters have been added to main class, including their parameters, and
    an array containing them

--- src/napakalaki/PruebaNapakalaki.java
+++ src/napakalaki/PruebaNapakalaki.java
@@ -6,6 +6,8 @@
 
 package napakalaki;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author David Vargas, Alicia Vílchez
@@ -54,12 +56,219 @@ public class PruebaNapakalaki {
         System.out.println(monstruo.toString());
         
         System.out.println();
+        System.out.println("****************************************");
+        System.out.println("// END OF TESTS //");
+        System.out.println("****************************************");
         
-        /**********************************************************************/
+        ////////////////////////////////////////////////////////////////////////
         // DEFINITION OF THE MONSTERS 
+        ////////////////////////////////////////////////////////////////////////
         
+        // Array which will store the monsters
+        ArrayList<Monster> monstruos = new ArrayList();
+        
+        // Array which will store the visible and hidden treasures
+        // that the player can obtain by killing a monster 
+        ArrayList<TreasureKind> tVisible = new ArrayList();
+        ArrayList<TreasureKind> tHidden = new ArrayList();
         
         /**********************************************************************/
+        // Byakhees de bonanza
+        
+        tVisible.add(TreasureKind.ARMOR);
+        tHidden.add(TreasureKind.ARMOR);
+        
+        BadConsequence badConsequence = new BadConsequence("Pierdes tu armadura"
+                + " visible y otra oculta", 0, tVisible, tHidden);
+        
+        Prize prize = new Prize(2, 1);
+        
+        monstruos.add(new Monster("Byakhees de bonanza", 8, badConsequence, 
+                prize));
+        
+        tVisible.clear();
+        tHidden.clear();
+        
+        /**********************************************************************/
+        // Chibithulhu
+        
+        tVisible.add(TreasureKind.HELMET);
+        
+        badConsequence = new BadConsequence("Embobados con el lindo" +
+                "primigenio te descartas de tu casco " +
+                "visible", 0, tVisible, tHidden);
+        
+        prize = new Prize(1, 1);
+        
+        monstruos.add(new Monster("Chibithulhu", 2, badConsequence, prize));
+        
+        tVisible.clear();
+        tHidden.clear();
+        
+        /**********************************************************************/
+        // El sopor de Dunwich
+        
+        tVisible.add(TreasureKind.SHOES);
+        
+        badConsequence = new BadConsequence("El primordial bostezo contagioso."
+                + " Pierdes el calzado visible", 0, tVisible, tHidden );
+        
+        prize = new Prize(1, 1);
+        
+        monstruos.add(new Monster ("El sopor de Dunwich", 2, badConsequence, 
+                prize));
+        
+        tVisible.clear();
+        tHidden.clear();
+        
+        /**********************************************************************/
+        // Ángeles de la noche ibicenca
+        
+        tVisible.add(TreasureKind.ONEHAND);
+        tHidden.add(TreasureKind.ONEHAND);
+        
+        badConsequence = new BadConsequence("Te atrapan para llevarte de fiesta"
+                + " y te dejan caer en mitad del vuelo. Descarta 1 mano visible"
+                + " y 1 mano oculta", 0, tVisible, tHidden);
+        
+        prize = new Prize(4, 1);
+        
+        monstruos.add(new Monster("Ángeles de la noche ibicenca", 14,
+                badConsequence, prize));
+        
+        tVisible.clear();
+        tHidden.clear();
+        
+        /**********************************************************************/
+        // El gorrón en el umbral
+        //
+        // We have assigned a large amount of visible treasures to lose in
+        // order to make the player lose all of them
+        
+        badConsequence = new BadConsequence("Pierdes todos tus tesoros "
+                + "visibles", 0, 20, 0);
+        
+        prize = new Prize(3, 1);
+        
+        monstruos.add(new Monster("El gorrón en el umbral", 10, badConsequence,
+                prize));
+        
+        /**********************************************************************/
+        // H.P Munchcraft
+        
+        tVisible.add(TreasureKind.ARMOR);
+        
+        badConsequence = new BadConsequence("Pierdes la armadura visible", 0,
+                tVisible, tHidden);
+        
+        prize = new Prize(2, 1);
+        
+        monstruos.add(new Monster("H.P Munchcraft", 6, badConsequence, prize));
+        
+        tVisible.clear();
+        
+        /**********************************************************************/
+        // Bichgooth
+        
+        tVisible.add(TreasureKind.ARMOR);
+        
+        badConsequence = new BadConsequence("Sientes bichos bajo la ropa. "
+                + "Descarta la armadura visible", 0, tVisible, tHidden);
+        
+        prize = new Prize(1, 1);
+        
+        monstruos.add(new Monster("Bichgooth", 2, badConsequence, prize));
+        
+        tVisible.clear();
+        
+        /**********************************************************************/
+        // El rey de rosa
+        
+        badConsequence = new BadConsequence("Pierdes 5 niveles y 3 tesoros "
+                + "visibles.", 5, 3, 0);
+        
+        prize = new Prize(4, 2);
+        
+        monstruos.add(new Monster("El rey de rosa", 13, badConsequence, prize));
+        
+        /**********************************************************************/
+        // La que redacta en las tinieblas
+        
+        badConsequence = new BadConsequence("Toses los pulmones y pierdes 2"
+                + " niveles", 2, 0, 0);
+        
+        prize = new Prize(1, 1);
+        
+        monstruos.add(new Monster("La que redacta en las tinieblas", 2, 
+                badConsequence, prize));
+        
+        /**********************************************************************/
+        // Los hondos
+        
+        badConsequence = new BadConsequence("Estos monstruos resultan bastante"
+                + " superficiales y te aburren mortalmente. Estás muerto", 
+                true);
+        
+        prize = new Prize(2, 1);
+        
+        monstruos.add(new Monster("Los hondos", 8, badConsequence, prize));
+        
+        /**********************************************************************/
+        // Semillas Cthulhu
+        
+        badConsequence = new BadConsequence("Pierdes 2 niveles y 2 tesoros "
+                + "ocultos", 2, 0, 2);
+        
+        prize = new Prize(2, 1);
+        
+        monstruos.add(new Monster("Semillas Cthulhu", 4, badConsequence, 
+                prize));
+        
+        /**********************************************************************/
+        // Dameargo
+        
+        tVisible.add(TreasureKind.ONEHAND);
+        
+        badConsequence = new BadConsequence("Te intentas escaquear. Pierdes "
+                + "una mano visible", 0, tVisible, tHidden);
+        
+        prize = new Prize(2, 1);
+        
+        monstruos.add(new Monster("Dameargo", 1, badConsequence, prize));
+        
+        tVisible.clear();
+        
+        /**********************************************************************/
+        // Pollipólipo volante
+        
+        badConsequence = new BadConsequence("Da mucho asquito. Pierdes 3 "
+                + "niveles", 3, 0, 0);
+        
+        prize = new Prize(1, 1);
+        
+        monstruos.add(new Monster("Pollipólipo volante", 3, badConsequence, 
+                prize));
+        
+        /**********************************************************************/
+        // Yskhtihyssg-Goth
+        
+        badConsequence = new BadConsequence("No le hace gracia que pronuncien"
+                + " mal su nombre. Estás muerto", true);
+        
+        prize = new Prize(3, 1);
+        
+        monstruos.add(new Monster("Yskhtihyssg-Goth", 12, badConsequence, 
+                prize));
+        
+        /**********************************************************************/
+        // Familia feliz
+        
+        badConsequence = new BadConsequence("La familia te atrapa. Estás "
+                + "muerto", true);
+        
+        prize = new Prize(4, 1);
+        
+        monstruos.add(new Monster("Familia feliz", 1, badConsequence, prize));
         
     }
     
