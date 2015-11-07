e7aa2d1931001bc336ebe86974ba217695507527
Alicia Vílchez Ceballos <aliavc96@correo.ugr.es>
2015-11-08 00:17:39 +0100

    Update Player.java

--- src/napakalaki/Player.java
+++ src/napakalaki/Player.java
@@ -34,19 +34,171 @@ public class Player {
     /**************************************************************************/
     
     //CONSTRUCTOR
-    
-     public Player(String name, int level, Player enemy, 
-             ArrayList<Treasure> hiddenTreasures, 
-             ArrayList<Treasure> visibleTreasures, 
-             BadConsequence pendingBadConsequence) {
-        
+     
+    public Player(String name) {
         this.name = name;
-        this.level = level;
-        this.enemy = enemy;
-        this.hiddenTreasures = hiddenTreasures;
-        this.visibleTreasures = visibleTreasures;
-        this.pendingBadConsequence = pendingBadConsequence;
     }
+    
+    // METHODS
+    
+    public String getName(){
+        return name;
+    }
+    
+    private void bringToLife(){
+    
+        /*
+        ...
+        */
+    
+    }
+    
+    private int getCombatLevel(){
+        return level;
+    }
+    
+    private void incrementLevels( int l ){
+        level = level + l;
+    } 
+    
+    private void decrementLevels( int l ){
+        level = level - l;
+    } 
+    
+    private void setPendingBadConsequence( BadConsequence b ){ 
+        pendingBadConsequence = b;
+    } 
+    
+    private void applyPrize( Monster m ){
+        /*
+        ...
+        ...
+        */
+    
+    } 
+    
+    private void applyBadConsequence( Monster m ){
+        /*
+        ...
+        ...
+        */
+    
+    } 
+    
+    private boolean canMakeTreasureVisible( Treasure t ){
+        /*
+        ...
+        ...
+        */
+    } 
+    
+    private int howManyVisibleTreasures( TreasureKind tKind ){
+        
+       return visibleTreasures.size();
+    
+    } 
+    
+    private void dieIfNoTreasures(){
+        /*
+        ...
+        */
+    
+    } 
+    
+    public boolean isDead(){
+        return dead;
+    } 
+    
+ 
+    public ArrayList<Treasure> getHiddenTreasures() {
+        return hiddenTreasures;
+    }
+
+    public ArrayList<Treasure> getVisibleTreasures() {
+        return visibleTreasures;
+    }
+    
+    public CombatResult combat( Monster m ){
+        /*
+        ...
+        */
+    }
+    
+    public void makeTreasureVisible(  Treasure t ){
+        /*
+        ...
+        .
+        .
+        */
+    }  
+    public void discardVisibleTreasure( Treasure t ){
+        /*
+        ...
+        ...
+        */
+    } 
+    
+    public void discardHiddenTreasure( Treasure t ){
+        /*
+        ...
+        ...
+        */
+    } 
+    
+    public boolean validState(){
+    
+        /*
+        ...
+        */
+    } 
+
+    public int getLevel() {
+        return level;
+    }
+    
+    public Treasure stealTreasure(){
+        /*
+        ...
+        ...
+        */
+    } 
+
+    public void setEnemy(Player enemy) {
+        this.enemy = enemy;
+    }
+    
+    private Treasure giveMeATreasure(){
+        /*
+        ...
+        ...
+        */
+    }
+    
+    public boolean canISteal(){
+        return canISteal;
+    }
+    
+    private boolean canYouGiveMeATreasure(){
+        /*
+        ...
+        ...
+        */
+    }
+    
+    private void haveStolen(){
+        /*
+        ...
+        ...
+        */
+    } 
+    
+    public void discardAllTreasures(){
+        /*
+        ...
+        ...
+        */
+    }
+    
     
     
 }
