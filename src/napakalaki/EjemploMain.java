567b5504423e96e01efa8ed461fd38fbfdcbee11
David Vargas <dvcarrillo@correo.ugr.es>
2016-01-12 17:48:14 +0100

    Started making GUI

--- src/napakalaki/EjemploMain.java
+++ src/napakalaki/EjemploMain.java
@@ -43,46 +43,46 @@ public class EjemploMain {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-      
-      // Maximum number of players that can play the game (adjust them according
-      // to the number of avaliable cards and treasures)
-      final int MAXPLAYERS = 3;
-      
-      System.out.println ("**********************************************************");
-      System.out.println ("*                    - NAPAKALAKI -                      *");
-      System.out.println ("*             BY DAVID VARGAS, ALICIA VILCHEZ            *");
-      System.out.println ("*                         2015                           *");
-      System.out.println ("*                                                        *");
-      System.out.println ("*              http://github.com/dvcarrillo              *");
-      System.out.println ("**********************************************************");
-      System.out.println ("v0.8-beta");
-      System.out.println ();
-      System.out.println ();
-      
-      Napakalaki game = Napakalaki.getInstance();
-      GameTester test = GameTester.getInstance();
-      
-      // Indicating the number of players or the need of help
-      Scanner capt = new Scanner(System.in);
-      int input;
-      
-      do {
-        System.out.print("Type the amount of players or press 0 for help: ");
-        input = capt.nextInt();
-        
-        if (input == 0)
-            showHelp();
-        if (input == 1)
-            System.out.println("There must be at least 2 players");
-        if (input > MAXPLAYERS)
-            System.out.println("There cannot be more than " + MAXPLAYERS + 
-                    " players");
-      } while ((input < 2) || (input > MAXPLAYERS));
-      
-      System.out.println();
-      
-      // The game begins with the desired players
-      test.play(game, input);
-    }
+//    public static void main(String[] args) {
+//      
+//      // Maximum number of players that can play the game (adjust them according
+//      // to the number of avaliable cards and treasures)
+//      final int MAXPLAYERS = 3;
+//      
+//      System.out.println ("**********************************************************");
+//      System.out.println ("*                    - NAPAKALAKI -                      *");
+//      System.out.println ("*             BY DAVID VARGAS, ALICIA VILCHEZ            *");
+//      System.out.println ("*                         2015                           *");
+//      System.out.println ("*                                                        *");
+//      System.out.println ("*              http://github.com/dvcarrillo              *");
+//      System.out.println ("**********************************************************");
+//      System.out.println ("v0.8-beta");
+//      System.out.println ();
+//      System.out.println ();
+//      
+//      Napakalaki game = Napakalaki.getInstance();
+//      GameTester test = GameTester.getInstance();
+//      
+//      // Indicating the number of players or the need of help
+//      Scanner capt = new Scanner(System.in);
+//      int input;
+//      
+//      do {
+//        System.out.print("Type the amount of players or press 0 for help: ");
+//        input = capt.nextInt();
+//        
+//        if (input == 0)
+//            showHelp();
+//        if (input == 1)
+//            System.out.println("There must be at least 2 players");
+//        if (input > MAXPLAYERS)
+//            System.out.println("There cannot be more than " + MAXPLAYERS + 
+//                    " players");
+//      } while ((input < 2) || (input > MAXPLAYERS));
+//      
+//      System.out.println();
+//      
+//      // The game begins with the desired players
+//      test.play(game, input);
+//    }
 }
